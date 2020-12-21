23fe9d2c54885d1f7abddb493bd11b6169c06a5a
Chichi-coder <lucie.macaigne@gmail.com>
2020-12-21 10:54:19 -0500

    added signup post controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,40 @@
-const { User } = require('../models/UserSchema');
+
+const User = require('../models/UserSchema');
 
 module.exports.homepageGet = (req, res, next) => {
-    res.render('index');
+    res.render('index', { title: 'Home' });
 };
 
 module.exports.loginGet = (req, res, next) => {
-    res.render('login');
+    res.render('login', { title: 'Login' });
 };
 
 module.exports.loginPost = (req, res, next) => {
     console.log('login post')
 };
 
-module.exports.signupGet = (res, req, next) => {
-    res.render('signup');
+module.exports.signupGet = (req, res, next) => {
+    res.render('signup', { title: 'Sign Up' });
 };
 
-module.exports.signupPost = (req, res, next) => {
-    console.log('signup post')
+module.exports.signupPost = async (req, res, next) => {
+    //const { username, email, password } = req.body;
+
+    try {
+        const user = await new User({ 
+            username: req.body.username,
+            email: req.body.email, 
+            password: req.body.password
+         }); 
+        user.save().then(newPost => {
+            res.send(newPost);
+        }, (err) => {
+            res.status(400).send(err);
+        });
+        
+    }
+    catch (err) {
+        console.log(err)
+    }
+    
 };
