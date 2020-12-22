2bd18f21ee8eb43e58990012b0186161e9885e4b
Chichi-coder <lucie.macaigne@gmail.com>
2020-12-22 17:49:54 -0500

    added logout and create tokens

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,12 @@
 const User = require('../models/UserSchema');
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.SECRET;
+const maxAge = 24 * 60 * 60;
+
+const createToken = (id) => {
+    return jwt.sign({ id }, secret, { expiresIn: maxAge })
+}
 
 module.exports.homepageGet = (req, res, next) => {
     res.render('index', { title: 'Home' });
@@ -14,6 +22,8 @@ module.exports.loginPost = async (req, res, next) => {
 
     try {
         const user = await User.checkPassword(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
         res.status(200).json({ user: user.username })
         console.log('logged in ')
         
@@ -29,18 +39,34 @@ module.exports.signupGet = (req, res, next) => {
 
 module.exports.signupPost = async (req, res, next) => {
 
+    // try {
+    //     const user = await new User({ 
+    //         username: req.body.username,
+    //         email: req.body.email, 
+    //         password: req.body.password
+    //      }); 
+    //     user.save().then(newPost => {
+    //         const token = createToken(user._id);
+    //         res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    //         res.send(newPost);
+            
+    //         //res.redirect(301, '/')
+    //     }, (err) => {
+    //         res.status(400).send(err)//.render('index');
+    //     });
+        
+    // }
+    // catch (err) {
+    //     console.log(err)
+    // }
+
+    const { username, email, password } = req.body;
+
     try {
-        const user = await new User({ 
-            username: req.body.username,
-            email: req.body.email, 
-            password: req.body.password
-         }); 
-        user.save().then(newPost => {
-            res.send(newPost);
-            //res.redirect(301, '/')
-        }, (err) => {
-            res.status(400).send(err)//.render('index');
-        });
+        const user = await User.create({ username, email, password });
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(201).json({ user: user._id });
         
     }
     catch (err) {
@@ -49,9 +75,9 @@ module.exports.signupPost = async (req, res, next) => {
     
 };
 
-module.exports.portalGet = (req, res, next) => {
-    res.render('portal');
-}
+// module.exports.portalGet = (req, res, next) => {
+//     res.render('portal', { title: 'My Portal' });
+// }
 
 module.exports.portalPut = (req, res, next) => {
     console.log('portal put');
@@ -59,4 +85,9 @@ module.exports.portalPut = (req, res, next) => {
 
 module.exports.portalDelete = (req, res, next) => {
     console.log('portal delete');
+}
+
+module.exports.logoutGet = (req, res, next) => {
+    res.cookie('jwt', {}, { maxAge: 1 });
+    res.redirect('/');
 }
