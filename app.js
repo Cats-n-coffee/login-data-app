616e7f9fcb142e3a06e901e4249c65596c075027
Chichi-coder <lucie.macaigne@gmail.com>
2020-12-22 12:29:31 -0500

    added helpers in handlebars setup

--- app.js
+++ app.js
@@ -21,7 +21,14 @@ app.engine('hbs', exphbs({
     defaultLayout: 'main',
     extname: '.hbs',
     layoutsDir: __dirname + '/views/layouts',
-    partialsDir: __dirname + '/views/partials'
+    partialsDir: __dirname + '/views/partials',
+    helpers: {
+        section: function(name, options) {
+            if (!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
 }));
 
 app.set('view engine', 'hbs');
