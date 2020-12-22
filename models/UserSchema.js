62518948e6b9fde8bb7fb0fe40cf45aba7284cd5
Chichi-coder <lucie.macaigne@gmail.com>
2020-12-22 12:24:50 -0500

    added password hash and login compare function

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -20,6 +23,29 @@ const UserSchema = new Schema({
         maxlength: 25
     }
 });
+
+UserSchema.pre('save', async function(next) {
+    try {
+        const salt = await bcrypt.genSalt();
+        this.password = await bcrypt.hash(this.password, salt);
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+});
+
+UserSchema.statics.checkPassword = async function(email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+        const match = await bcrypt.compare(password, user.password);
+        if (match) {
+            return user;
+        }
+        throw Error('incorrect password');
+    }
+    throw Error('incorrect email');
+    
+}
 
 const User = mongoose.model('user', UserSchema);
 
