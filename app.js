2c46a8818cc1c5182963a11b1ebd0a0315b5d111
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-01-31 23:00:11 -0500

    changed port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { verifyToken, requireToken } = require('./middleware/authMiddleware');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 5000;
 
 const mongoUser = process.env.MONGO_USER ;
 const mongoPassword = process.env.MONGO_PASSWORD ;
